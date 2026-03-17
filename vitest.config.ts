ba769181f2a06f5e9f412651b156413efc88fa88
Eli Revah <elirev4@gmail.com>
2026-03-17 11:12:20 +0100

    fix: define build env constants in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config';
 import { resolve } from 'path';
 
 export default defineConfig({
+  define: {
+    __BUILD_ENV_AGENT_WEBSOCKET_URL__: JSON.stringify(''),
+    __BUILD_ENV_API_URL__: JSON.stringify(''),
+  },
   test: {
     globals: false,
     environment: 'node',
